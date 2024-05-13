fa2719542d9c9731731fe47106fc4ca2ac8af2e9
Emily Wang <emilywang0416@gmail.com>
2024-05-13 14:30:01 -0700

    all project files

---
+++ WeatherApp/Views/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  WeatherApp
+//
+//  Created by Emily Wang on 2024-05-10.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
---
+++ WeatherApp/Views/WelcomeView.swift
@@ -0,0 +1,40 @@
+//
+//  WelcomeView.swift
+//  WeatherApp
+//
+//  Created by Emily Wang on 2024-05-10.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationManager: LocationManager
+    
+    var body: some View {
+        VStack {
+            VStack(spacing:20) {
+                Text("Welcome to the Weather App")
+                    .bold()
+                    .font(.title)
+                
+                Text("Please share your current location to get the weather in your area")
+                    .padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation) {
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+    
+#Preview {
+    WelcomeView()
+}
